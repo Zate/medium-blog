88cb7502093119538ee722d87cd950f5c201e77e
Zate <zate75@gmail.com>
2018-02-16 09:39:20 -0600

    skeleton

---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"log"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lmicroseconds) //| log.Lshortfile)
+	log.Println("Medium Blog Bridge Coming Online ....")
+
+}
